e724456355e8b043be80be68d7c3193f2d8e371c
mthiranat <mthiranat@ptnicec.ac.th>
2018-11-08 14:32:17 +0700

    send project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
+  }
+
+  showAlert1() {
+    const alert = this.alertCtrl.create({
+      title: "แจ้งเตือนการใช้งาน",
+      subTitle: "ของระบบ",
+      buttons: [
+        "OK"
+      ]
+    });
+    alert.present();
+  }
+
+  showAlert2() {
+    const alert = this.alertCtrl.create({
+      title: "โปรดใส่รหัสผ่านที่สมัครไว้",
+      subTitle: "จำนวน 6 ตัวขึ้นไป",
+      inputs: []
+    });
+    alert.present();
   }
 
 }
